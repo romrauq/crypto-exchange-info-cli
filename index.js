f7b0e6a103239615240bf02acf10f6ad9d31b11e
John Quarmor <johnequarmor@gmail.com>
2025-05-25 22:07:18 +0000

    Refactored script to import `exchange` class from ./config directory.

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const ccxt = require("ccxt"); // Import ccxt module.
 const readline = require("readline-sync");
-require("dotenv").config();
-
-const exchange = new ccxt.binance({
-	apiKey: process.env.BINANCE_TESTNET_API_KEY,
-	secret: process.env.BINANCE_TESTNET_API_SECRET,
-	enableRateLimit: true,
-	options: {
-		defaultType: "spot",
-		recvWindow: 10000,
-		adjustForTimeDifference: true,
-	},
-});
-exchange.setSandboxMode(true); // Enables testnet mode.
+const { exchange } = require("./config/config");
 
 (async () => {
 	const markets = await exchange.load_markets();
