9bc770411bcfaffc0c8ca794dada6eed2ee5ba89
John Quarmor <johnequarmor@gmail.com>
2025-05-26 12:02:50 +0000

    Added function that filters symbols based on quote id specified.

--- functions/cliFunctions.js
+++ functions/cliFunctions.js
@@ -19,4 +19,33 @@ const checkBase = async (_exchange) => {
 	}
 };
 
-module.exports = { checkBase };
+// User enters a quote id & an array containing objects of symbols ending with the specified quote is returned:
+let getUSDTMarkets = async (_exchange) => {
+	await _exchange.loadMarkets(); // Loads available markets data of the exchange class.
+	const markets = await _exchange.fetchMarkets();
+
+	let quote_input = readline.question("Enter quote currency: ").trim(); // Get base input value from user.
+	let quote_uppercase = quote_input.toUpperCase();
+	let endQoute = `/${quote_uppercase}`;
+
+	let target_symbols = markets.filter((item) => item.symbol.endsWith(endQoute));
+	// console.log(target_symbols);
+
+	if (target_symbols.length !== 0) {
+		let count = 1;
+
+		// Console log for test purposes:
+		// Object.keys(target_symbols).forEach((key) => {
+		// 	console.log(count, target_symbols[key].symbol);
+		// 	count++;
+		// });
+
+		return target_symbols; // return array.
+	} else {
+		console.log(`There are no symbols with the end quote "${endQoute}"`);
+	}
+
+	console.log("Total Number of Symbols:", target_symbols.length);
+};
+
+module.exports = { checkBase, getUSDTMarkets };
