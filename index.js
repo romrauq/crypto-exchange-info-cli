9faf6266761ad68d907ab68aa4f810988c64d377
John Quarmor <johnequarmor@gmail.com>
2025-05-27 22:50:58 +0000

    Script to test the imported functions: - getUSDTMarkets - getSymbols

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const readline = require("readline-sync");
 const { exchange } = require("./config/config");
+const { getUSDTMarkets } = require("./functions/cliFunctions");
+const { getSymbols } = require("./functions/cliFunctions");
 
 (async () => {
-	const markets = await exchange.load_markets();
-	let base_input = readline.question("Enter Base Currency: ").trim(); // Get base input value from user.
-	let base_uppercase = base_input.toUpperCase();
-	let symbol = `${base_uppercase}/USDT`;
-
-	if (markets[symbol]) {
-		try {
-			let symbol_structure = exchange.markets[symbol]; // fixed property name from market to markets
-			console.log(symbol_structure);
-		} catch (error) {
-			console.log("Failed to retrieve symbol structure:", error);
-		}
-	} else {
-		console.log(`The symbol '${symbol}' is not within ${exchange.name}'s list of markets.`);
-	}
+	let usdtMarkets = await getUSDTMarkets(exchange);
+	let symbolsArray = await getSymbols(exchange, usdtMarkets);
+	console.log(usdtMarkets.length);
+	console.log(symbolsArray.length);
 })();
