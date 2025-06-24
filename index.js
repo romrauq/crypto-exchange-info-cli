b9623f833279e11a28eb64dc4ae2f1787dc5de9b
John Quarmor <johnequarmor@gmail.com>
2025-06-24 14:43:59 +0000

    Added script to get high volume pairs of specified quote asset.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const { exchange } = require("./config/config");
 const { getUSDTMarkets } = require("./functions/cliFunctions");
 const { getSymbols } = require("./functions/cliFunctions");
+const { getSymbolsVolumes, getHighVolumes } = require("./functions/getDataFunctions");
 
 (async () => {
 	let usdtMarkets = await getUSDTMarkets(exchange);
 	let symbolsArray = await getSymbols(exchange, usdtMarkets);
+
+	let symbolsData = await getSymbolsVolumes(exchange, symbolsArray);
+	let highVolumes = getHighVolumes(symbolsData, 100000000);
+
 	console.log(usdtMarkets.length);
 	console.log(symbolsArray.length);
+	console.log("Amount remaining: " + highVolumes.length);
+	console.log(highVolumes);
 })();
